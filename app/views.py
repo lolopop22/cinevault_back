18619e476350839ccdf8a6678e7b89e043bba510
lolopop22 <59579270+lolopop22@users.noreply.github.com>
2025-06-11 00:07:40 +0200

    feat(api): feat/US3-1 - Implémentation du détail des films et tests unitaires

--- app/views.py
+++ app/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 from .models import Movie
-from .serializers import MovieSerializer
+from .serializers import MovieListSerializer, MovieDetailSerializer
 
 
 class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
-    serializer_class = MovieSerializer
+    serializer_class = MovieListSerializer
+    detail_serializer_class = MovieDetailSerializer
+
+    def get_serializer_class(self):
+        if self.action == "retrieve":
+            return self.detail_serializer_class
+        return super().get_serializer_class()
